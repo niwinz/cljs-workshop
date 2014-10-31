a6e43e2d9ba993d90efdd9f74527e53201fc834b
Andrey Antukh <niwi@niwi.be>
2014-10-31 17:42:31 +0100

    Prepare first step.

--- src/clj/cljsworkshop/core.clj
+++ src/clj/cljsworkshop/core.clj
@@ -6,15 +6,21 @@
             [compojure.response :refer [render]]
             [clojure.java.io :as io]))
 
+;; Simple function that works as controller
+;; It should return a proper response. In our
+;; case it returns a content of static index.html.
 (defn home
   [req]
   (render (io/resource "index.html") req))
 
+
+;; Routes definition
 (defroutes app
   (GET "/" [] home)
   (route/resources "/static")
   (route/not-found "<h1>Page not found</h1>"))
 
+;; Application entry point
 (defn -main
   [& args]
   (-> app
