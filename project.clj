1885289262a60ec7e071764c795d7180f7104c98
Andrey Antukh <niwi@niwi.be>
2014-11-01 16:10:12 +0100

    Improvements.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject cljsworkshop "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "BSD (2-Clause)"
+            :url "http://opensource.org/licenses/BSD-2-Clause"}
   :dependencies [[org.clojure/clojure "1.6.0"]
 
                  ;; Backend dependencies
