978f09a66112a9a87feed7b6242b80596b1966a8
Andrey Antukh <niwi@niwi.be>
2014-11-01 12:24:37 +0100

    First events example.

--- src/cljs/cljsworkshop/core.cljs
+++ src/cljs/cljsworkshop/core.cljs
@@ -1,12 +1,22 @@
-(ns cljsworkshop.core)
-
-(defn set-html! [el content]
-  (set! (.-innerHTML el) content))
+(ns cljsworkshop.core
+  (:require [goog.events :as events]
+            [goog.dom :as dom]))
 
 (defn main
   []
-  (let [content "Hellow World from Clojure Script"
-        element    (aget (js/document.getElementsByTagName "main") 0)]
-    (set-html! element content)))
+  (let [counter  (atom 0)
+        button  (dom/getElement "button")
+        display (dom/getElement "clicksnumber")]
+
+    ;; Set initial value
+    (set! (.-innerHTML display) @counter)
+
+    ;; Assign event listener
+    (events/listen button "click"
+                   (fn [event]
+                     ;; Increment the value
+                     (swap! counter inc)
+                     ;; Set new value in display element
+                     (set! (.-innerHTML display) @counter)))))
 
 (main)
