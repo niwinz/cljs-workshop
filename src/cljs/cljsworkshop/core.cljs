8c274efd9b057dab1904ff938404d9307ce5ae41
Andrey Antukh <niwi@niwi.be>
2014-12-08 01:58:45 +0100

    Add partial implementation of timetraveling tasklist app for step7.

--- src/cljs/cljsworkshop/core.cljs
+++ src/cljs/cljsworkshop/core.cljs
@@ -9,25 +9,76 @@
 
 (enable-console-print!)
 
-(defonce state {:message "Hello world from global state."})
+(defonce tasklist-state (atom {:entries []}))
+(defonce undo-state (atom {:entries [@tasklist-state]
+                           :index nil}))
 
-(defn mycomponent
+(add-watch tasklist-state :history
+  (fn [_ _ _ n]
+    (println "Event:" n)
+    (let [entries (:entries @undo-state)]
+      (when-not (= (last entries) n)
+        (swap! undo-state
+               (fn [state]
+                 (update-in state [:entries] conj n)))))))
+
+(defn tasklist
   [app owner]
   (reify
     om/IDisplayName
     (display-name [_]
-      "my-component")
+      "tasklist")
 
-    om/IInitState
-    (init-state [_]
-      {:message "Hello world from local state"})
+    om/IRender
+    (render [_]
+      (let [entries (:entries app)]
+        (html
+         [:section {:style {:margin-top "20px"
+                            :padding "5px"
+                            :border "1px solid #ddd"}}
+          [:section.title
+           [:strong "Task list:"]]
+          [:section.input
+           [:form {:on-submit (fn [e]
+                                (.preventDefault e)
+                                (let [input (-> (.-target e)
+                                                (.querySelector "[name=subject]"))
+                                      task  {:subject (.-value input)
+                                             :state :todo}]
+                                  (set! (.-value input) "")
+                                  (om/transact! app :entries #(conj % task))))}
+            [:input {:type "text"
+                     :name "subject"
+                     :placeholder "Write your task name..."}]
+            [:input {:type "submit"
+                     :defaultValue "Foo"}]]]
+          [:section.list {:style {:margin-top "10px"}}
+           (if (empty? entries)
+             [:span "No items on the task list..."]
+             [:ul
+              (for [item entries]
+                (condp = (:state item)
+                  :done [:li {:style {:text-decoration "line-through"}}
+                         (:subject item)]
+                  :todo [:li (:subject item)]))])]])))))
 
-    om/IRenderState
-    (render-state [_ {:keys [message]}]
-      (html [:section
-             [:div message]
-             [:div (:message app)]]))))
 
+(defn undo
+  [app owner]
+  (reify
+    om/IRender
+    (render [_]
+      (html
+       [:section.undo {:style {:padding "5px"
+                               :border "1px solid #ddd"}}
+        [:section.buttons
+         [:input {:type "button" :default-value "Undo"
+                  :on-click (fn [event]
+                              (when (> (count (:entries @app)) 1)
+                                (om/transact! app :entries pop)
+                                (reset! tasklist-state (last (:entries @undo-state)))))}]]]))))
 
-(let [el (gdom/getElement "app")]
-  (om/root mycomponent state {:target el}))
+(let [undoel (gdom/getElement "undo")
+      tasklistel (gdom/getElement "tasklist")]
+  (om/root undo undo-state {:target undoel})
+  (om/root tasklist tasklist-state {:target tasklistel}))
