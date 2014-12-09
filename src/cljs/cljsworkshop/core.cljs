d14c8429d4742bb635217be08048b1ffb29a54cb
Andrey Antukh <niwi@niwi.be>
2014-12-09 19:58:12 +0100

    Fix step1

--- src/cljs/cljsworkshop/core.cljs
+++ src/cljs/cljsworkshop/core.cljs
@@ -1,22 +1,12 @@
-(ns cljsworkshop.core
-  (:require [goog.events :as events]
-            [goog.dom :as dom]))
+(ns cljsworkshop.core)
+
+(defn set-html! [el content]
+  (set! (.-innerHTML el) content))
 
 (defn main
   []
-  (let [counter  (atom 0)
-        button  (dom/getElement "button")
-        display (dom/getElement "clicksnumber")]
-
-    ;; Set initial value
-    (set! (.-innerHTML display) @counter)
-
-    ;; Assign event listener
-    (events/listen button "click"
-                   (fn [event]
-                     ;; Increment the value
-                     (swap! counter inc)
-                     ;; Set new value in display element
-                     (set! (.-innerHTML display) @counter)))))
+  (let [content "Hello World from Clojure Script"
+        element (aget (js/document.getElementsByTagName "main") 0)]
+    (set-html! element content)))
 
 (main)
