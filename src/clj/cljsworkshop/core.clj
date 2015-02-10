c474d8a427574fc6e27fab3672de123eea0813d1
Andrey Antukh <niwi@niwi.be>
2015-02-10 23:22:12 +0100

    Update jetty server.

--- src/clj/cljsworkshop/core.clj
+++ src/clj/cljsworkshop/core.clj
@@ -1,5 +1,6 @@
 (ns cljsworkshop.core
-  (:require [ring.adapter.jetty9 :refer [run-jetty]]
+  (:require [qbits.jet.server :refer [run-jetty]]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [compojure.core :refer :all]
             [compojure.route :as route]
             [compojure.response :refer [render]]
@@ -21,7 +22,6 @@
 ;; Application entry point
 (defn -main
   [& args]
-  (run-jetty app {:port 5050}))
-
-
+  (let [app (wrap-defaults app site-defaults)]
+    (run-jetty {:ring-handler app :port 5050})))
 
