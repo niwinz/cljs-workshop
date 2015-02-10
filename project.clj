ad3c24df8a6c42a5c9f6f9c8b89faa26e724097c
Andrey Antukh <niwi@niwi.be>
2015-02-10 23:22:12 +0100

    Update jetty server.

--- project.clj
+++ project.clj
@@ -7,15 +7,9 @@
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
 
                  ;; Backend dependencies
-                 [compojure "1.2.1"]
-                 [ring/ring-core "1.3.1" :exclusions [javax.servlet/servlet-api]]
-                 [ring/ring-servlet "1.3.1" :exclusions [javax.servlet/servlet-api]]
-                 [ring/ring-defaults "0.1.2"]
-
-                 [javax.servlet/javax.servlet-api "3.1.0"]
-                 [info.sunng/ring-jetty9-adapter "0.7.2"]
                  [com.cemerick/piggieback "0.1.3"]
                  [weasel "0.4.2"]
+                 [compojure "1.3.1"]
 
                  ;; Frontend dependencies
                  [org.clojure/clojurescript "0.0-2411"]
@@ -23,6 +17,12 @@
                  [sablono "0.2.22"]
                  [om "0.8.0-alpha2"]
                  [hodgepodge "0.1.0"]]
+
+                 [ring/ring-core "1.3.2" :exclusions [javax.servlet/servlet-api]]
+                 [ring/ring-servlet "1.3.2" :exclusions [javax.servlet/servlet-api]]
+                 [ring/ring-defaults "0.1.2" :exclusions [javax.servlet/servlet-api]]
+
+                 [cc.qbits/jet "0.5.4"]]
 
   :source-paths ["src/clj"]
   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
