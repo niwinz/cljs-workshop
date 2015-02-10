061c5ca602d15d9b3429cd75c8fcca87025758f8
Andrey Antukh <niwi@niwi.be>
2015-02-10 23:45:56 +0100

    Update cljs compiler.

--- project.clj
+++ project.clj
@@ -10,11 +10,11 @@
                  [compojure "1.3.1"]
 
                  ;; Frontend dependencies
-                 [org.clojure/clojurescript "0.0-2371"]
-                 [secretary "1.2.1"]
                  [sablono "0.2.22"]
-                 [om "0.8.0-alpha2"]
-                 [prismatic/om-tools "0.3.6"]]
+                 [org.omcljs/om "0.8.8"]
+                 [prismatic/om-tools "0.3.6"]
+                 [secretary "1.2.1"]
+                 [org.clojure/clojurescript "0.0-2816"]
 
                  [ring/ring-core "1.3.2" :exclusions [javax.servlet/servlet-api]]
                  [ring/ring-servlet "1.3.2" :exclusions [javax.servlet/servlet-api]]
@@ -25,14 +25,14 @@
   :source-paths ["src/clj"]
 
   :main cljsworkshop.core
-  :plugins [[lein-cljsbuild "1.0.3"]]
+  :plugins [[lein-cljsbuild "1.0.4"]]
   :cljsbuild {:builds
               [{:id "app"
                 :source-paths ["src/cljs"]
-                :compiler {:optimizations :whitespace
-                           :pretty-print true
-                           :preamble ["react/react.min.js"]
-                           :output-to "resources/public/js/app.js"
-                           ;; :output-dir "resources/public/js/"
-                           ;; :source-map "resources/public/js/app.js.map"
-                           }}]})
+                :compiler {:output-to "resources/public/js/app.js"
+                           :output-dir "resources/public/js/out"
+                           :source-map true
+                           :optimizations :none
+                           :asset-path "/static/js/out"
+                           :main "cljsworkshop.core"
+                           :pretty-print true}}]})
