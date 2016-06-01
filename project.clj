a0a0f9a834713ec9ecf3dc550966e6c366508bd9
Andrey Antukh <niwi@niwi.nz>
2016-06-01 17:43:11 +0300

    Update dependencies.

--- project.clj
+++ project.clj
@@ -3,16 +3,17 @@
   :url "http://example.com/FIXME"
   :license {:name "BSD (2-Clause)"
             :url "http://opensource.org/licenses/BSD-2-Clause"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
 
                  ;; Backend dependencies
-                 [compojure "1.3.1"]
+                 [compojure "1.5.0"]
 
-                 [ring/ring-core "1.3.2" :exclusions [javax.servlet/servlet-api]]
-                 [ring/ring-servlet "1.3.2" :exclusions [javax.servlet/servlet-api]]
-                 [ring/ring-defaults "0.1.2" :exclusions [javax.servlet/servlet-api]]
+                 [ring/ring-core "1.4.0" :exclusions [javax.servlet/servlet-api]]
+                 [ring/ring-servlet "1.4.0" :exclusions [javax.servlet/servlet-api]]
+                 [ring/ring-defaults "0.2.0" :exclusions [javax.servlet/servlet-api]]
 
-                 [cc.qbits/jet "0.5.4"]]
+                 [cc.qbits/jet "0.7.8"]]
 
+  :plugins [[lein-ancient "0.6.10"]]
   :source-paths ["src/clj"]
   :main cljsworkshop.core)
